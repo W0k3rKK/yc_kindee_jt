58952fb4465d2fe633cd2ba8dd3a0b883c792b30
W0k3rKK <570488980@qq.com>
2025-07-07 17:34:50 +0800

    feat(report): 优化融资分类统计表
    - 新增合计行功能
    - 修复利息计算问题- 优化平均利率成本计算- 调整数据筛选条件
    - 重构部分代码结构

--- code/tmc/p8z9-jtuat-tmc-cfm/src/main/java/p8z9/jtuat/tmc/cfm/plugin/report/FinancingClassificationStatisticsDataListPlugin.java
+++ code/tmc/p8z9-jtuat-tmc-cfm/src/main/java/p8z9/jtuat/tmc/cfm/plugin/report/FinancingClassificationStatisticsDataListPlugin.java
@@ -18,12 +18,21 @@ import kd.tmc.cfm.report.helper.TradeFinanceFilterHelper;
 import kd.tmc.cfm.report.helper.TradeFinanceRptHelper;
 import kd.tmc.fbp.common.enums.BillStatusEnum;
 import kd.tmc.fbp.common.util.EmptyUtil;
+import p8z9.jtuat.tmc.cfm.plugin.report.util.FinanceHelper;
 
 import java.util.*;
 
 /**
  * 融资分类统计表（本年融资情况汇总表）
  * 报表取数插件
+ * <p>
+ * 过滤条件查询年份明细至年月日  ---
+ * 占比四舍五入保留四位小数    ---
+ * 利息需除以一百，因为字段为百分比 ---
+ * 按融资主体维度，金控资产管理有限公司不存在本金却存在利息（本位币）   ---
+ * 平均利率成本应该为小数 ---
+ * 融资余额、平均利率成本、本金未正确带出 ---
+ * </p>
  */
 public class FinancingClassificationStatisticsDataListPlugin extends AbstractReportListDataPlugin implements Plugin {
 
@@ -49,37 +58,65 @@ public class FinancingClassificationStatisticsDataListPlugin extends AbstractRep
 
         // 根据不同统计维度，进行不同数据查询及组装
         DataSet dataSet = null;
-        switch (value_dimension) {
-            case "financing_model":
-            case "financing_subject":
-                String groupByField = null;
-                if ("financing_model".equals(value_dimension)) {
-                    // 融资模式
-                    // 根据融资模式（融资品种一级级次）进行分组并汇总金额
-                    groupByField = "p8z9_rzms";
-                } else {
-                    // 融资主体
-                    // 根据融资主体进行分组并汇总金额
-                    groupByField = "p8z9_org";
-                }
-                // 获取进行初步处理包含基础字段数据
-                DataSet DS = this.getModelDs(queryParam);
-                // 按维度小计
-                dataSet = DS.copy().groupBy(new String[]{groupByField})
-                        .sum("p8z9_baseamt")// 融资余额（本位币）汇总
-                        .sum("p8z9_baseprincipal")// 本金（本位币）汇总
-                        .sum("p8z9_baseinterest")// 利息（本位币）汇总
-                        .finish();
-                dataSet = dataSet.addField("1", "p8z9_basecur");// 本位币赋值
-                break;
-            case "credit_method":
-                // 增信方式
-                // 根据担保方式进行分组并汇总金额
-                DataSet methodDs = this.getMethodDs();
-                return methodDs;
+        List<String> sumFields;
+        try {
+
+            switch (value_dimension) {
+                case "financing_model":
+                case "financing_subject":
+                    String groupByField;
+                    if ("financing_model".equals(value_dimension)) {
+                        // 融资模式
+                        // 根据融资模式（融资品种一级级次）进行分组并汇总金额
+                        groupByField = "p8z9_rzms";
+                    } else {
+                        // 融资主体
+                        // 根据融资主体进行分组并汇总金额
+                        groupByField = "p8z9_org";
+                    }
+                    // 获取进行初步处理包含基础字段数据
+                    DataSet DS = this.getModelDs(queryParam);
+                    // 按维度小计
+                    dataSet = DS.copy().groupBy(new String[]{groupByField})
+                            .sum("p8z9_baseamt")// 融资余额（本位币）汇总
+                            .sum("p8z9_baseprincipal")// 本金（本位币）汇总
+                            .sum("p8z9_baseinterest")// 利息（本位币）汇总
+                            .finish();
+                    dataSet = dataSet.addField("1", "p8z9_basecur");// 本位币赋值
+                    dataSet = dataSet.addField("'0'", "p8z9_sumlevel");// 合计排序
+                    dataSet = dataSet.addField("CASE WHEN p8z9_baseprincipal <> 0 AND p8z9_baseprincipal IS NOT NULL THEN p8z9_baseinterest/p8z9_baseprincipal ELSE 0 END", "p8z9_avg_interestrate");//平均利率成本 = 利息（本位币） / 本金（本位币）
+
+                    // 需要合计字段
+                    sumFields = new ArrayList<>(Arrays.asList("p8z9_baseamt", "p8z9_baseprincipal", "p8z9_baseinterest"));
+                    // 融资模式、融资主体合计行
+                    dataSet = this.addSumRow(dataSet, sumFields);
+
+                    break;
+                case "credit_method":
+                    // 增信方式
+                    // 根据担保方式进行分组并汇总金额
+                    dataSet = this.getMethodDs();
+                    dataSet = dataSet.addField("'0'", "p8z9_sumlevel");// 合计排序
+
+                    // 需要合计字段
+                    sumFields = new ArrayList<>(Arrays.asList("p8z9_baseamt"));
+                    // 增信方式合计行
+                    dataSet = this.addSumRow(dataSet, sumFields);
+                    break;
+            }
+        } catch (Exception e) {
+            logger.error(e);
         }
 
         return dataSet;
+    }
+
+    private DataSet addSumRow(DataSet dataSet, List<String> sumFields) {
+        DataSet totalSumDS = FinanceHelper.buildSummaryDS(dataSet, sumFields, null);
+        totalSumDS = totalSumDS.updateField("p8z9_sumlevel", "'2'");
+        DataSet afterSumDS = dataSet.union(totalSumDS);
+
+        return afterSumDS;
     }
 
     private DataSet getMethodDs() {
@@ -97,19 +134,20 @@ public class FinancingClassificationStatisticsDataListPlugin extends AbstractRep
     private DataSet queryMethodDS() {
         QFilter billStatusQ = new QFilter("gcontract.billstatus", "=", BillStatusEnum.AUDIT.getValue());// 担保合同.单据状态 = 已审核
         QFilter bizStatusQ = new QFilter("gcontract.bizstatus", "=", "doing");// 担保合同.业务状态 = 执行中
-        QFilter dateQ = new QFilter("gcontract.bizdate", ">=", DateUtil.beginOfYear(this.cutoffdate));// 起始日期大于等于本年
+        QFilter startDateQ = new QFilter("gcontract.bizdate", ">=", DateUtil.beginOfYear(this.cutoffdate));// 起始日期大于等于本年
+        QFilter endDateQ = new QFilter("gcontract.bizdate", ">=", DateUtil.beginOfYear(this.cutoffdate));// 起始日期大于等于本年
         String selFields = "gdebtcurrency, gdebtbalance, gcontract.guaranteevarieties.name AS p8z9_guarantee";
         List<DataSet> dataSetList = new ArrayList<>();
 
         // 将包含保证的担保品种都归为 保证
         DataSet ensureDS = QueryServiceHelper.queryDataSet(this.getClass().getName(), "gm_guaranteeuse", selFields,
-                new QFilter[]{bizStatusQ, billStatusQ, dateQ, new QFilter("gcontract.guaranteevarieties.name", "like", "保证%")}, null);
+                new QFilter[]{bizStatusQ, billStatusQ, startDateQ, endDateQ, new QFilter("gcontract.guaranteevarieties.name", "like", "保证%")}, null);
         ensureDS = ensureDS.updateField("p8z9_guarantee", "'保证'");
         dataSetList.add(ensureDS);
 
         // 其余担保品种
         DataSet otherDS = QueryServiceHelper.queryDataSet(this.getClass().getName(), "gm_guaranteeuse", selFields,
-                new QFilter[]{bizStatusQ, billStatusQ, dateQ, new QFilter("gcontract.guaranteevarieties.name", "not like", "保证%")}, null);
+                new QFilter[]{bizStatusQ, billStatusQ, startDateQ, endDateQ, new QFilter("gcontract.guaranteevarieties.name", "not like", "保证%")}, null);
         dataSetList.add(otherDS);
 
         DataSet methodDS = EmptyUtil.isEmpty(dataSetList) ? null : dataSetList.stream().reduce(DataSet::union).get();
@@ -121,7 +159,7 @@ public class FinancingClassificationStatisticsDataListPlugin extends AbstractRep
         // 计算金额本位币、金额公式
         String curUnit = paramMap.get("p8z9_filter_currencyunit").toString();// 货币单位
         String curField = "gdebtcurrency";
-        DataSet rateDs = TradeFinanceFilterHelperExt.getExChangeRateDs(methodDS, curField, 1L, this.cutoffdate);// 汇率
+        DataSet rateDs = FinanceHelper.getExChangeRateDs(methodDS, curField, 1L, this.cutoffdate);// 汇率
         List<String> joinSelPropList = new ArrayList<>();
         Collections.addAll(joinSelPropList, methodDS.getRowMeta().getFieldNames());
         joinSelPropList.add("tarcurrency");
@@ -149,44 +187,36 @@ public class FinancingClassificationStatisticsDataListPlugin extends AbstractRep
         // 处理金额等数据
         modelDS = this.calModelAmt(modelDS);
         // 将融资品种一级级次名，加入modelDS
-        modelDS = varietyDs.join(modelDS).on("finproduct.id", "finproduct.id").select(modelDS.getRowMeta().getFieldNames(), new String[]{"category AS p8z9_rzms"}).finish();
+        modelDS = modelDS.join(varietyDs).on("finproduct.id", "finproduct.id").select(modelDS.getRowMeta().getFieldNames(), new String[]{"category AS p8z9_rzms"}).finish();
 
         return modelDS;
     }
 
     private DataSet calModelAmt(DataSet modelDS) {
-        // 单据id集合 全部
-        // List<Long> allModelLoanBillIds = TradeFinanceFilterHelper.getloanBillIds(modelDS, "loanbillid");
-        // 融资余额（原币） 取时点的未还本金汇总，全部
-        // DataSet allModelPayAmtDS = TradeFinanceFilterHelperExt.enotrePayAmtDS(allModelLoanBillIds, cutoffdate, "loanbillid", "p8z9_amt", this.getClass());
-        // 将融资余额（原币）join进主DataSet
-        // modelDS = modelDS.join(allModelPayAmtDS).on("loanbillid", "loanbillid").select(modelDS.getRowMeta().getFieldNames(), new String[]{"p8z9_amt"}).finish();
 
         // 单据id集合 年度
-        // DataSet yearModelDS = modelDS.copy().filter("bizdate >= '" + DateUtil.beginOfYear(this.cutoffdate) + "'");
         List<Long> yearModelLoanBillIds = TradeFinanceFilterHelper.getloanBillIds(modelDS, "loanbillid");
         // 本金（原币） 取时点的未还本金汇总，年度
-        DataSet yearPayAmtDS = TradeFinanceFilterHelperExt.enotrePayAmtDS(yearModelLoanBillIds, this.cutoffdate, "loanbillid", "p8z9_amt", this.getClass());
+        DataSet yearPayAmtDS = FinanceHelper.enotrePayAmtDS(yearModelLoanBillIds, this.cutoffdate, "loanbillid", "p8z9_amt", this.getClass());
         // 将本金（原币）join进主DataSet
         modelDS = modelDS.join(yearPayAmtDS).on("loanbillid", "loanbillid").select(modelDS.getRowMeta().getFieldNames(), new String[]{"p8z9_amt"}).finish();
 
         // 计算金额本位币、金额公式
         String curUnit = paramMap.get("p8z9_filter_currencyunit").toString();// 货币单位
         String curField = "p8z9_srccur";
-        DataSet rateDs = TradeFinanceFilterHelperExt.getExChangeRateDs(modelDS, curField, 1L, this.cutoffdate);
+        DataSet rateDs = FinanceHelper.getExChangeRateDs(modelDS, curField, 1L, this.cutoffdate);
         modelDS = modelDS.join(rateDs).on("p8z9_srccur", "tarcurrency").select(modelDS.getRowMeta().getFieldNames(), new String[]{"rate"}).finish();
         modelDS = modelDS.addField("p8z9_amt * rate / " + curUnit, "p8z9_baseamt");//融资余额（本位币）
         modelDS = modelDS.addField("p8z9_baseamt", "p8z9_baseprincipal");//本金（本位币） 目前等于融资余额（本位币）
-        modelDS = modelDS.addField("drawamount * p8z9_tzll * rate / " + curUnit, "p8z9_baseinterest");//利息（本位币）
-        modelDS = modelDS.addField("CASE WHEN p8z9_baseprincipal <> 0 AND p8z9_baseprincipal IS NOT NULL THEN p8z9_baseinterest / p8z9_baseprincipal ELSE 0 END", "p8z9_avg_interestrate");//平均利率成本 = 利息（本位币） / 本金（本位币）
+        modelDS = modelDS.addField("drawamount * p8z9_tzll * rate / 100 /" + curUnit, "p8z9_baseinterest");//利息（本位币）
 
         return modelDS;
     }
 
     private DataSet queryModelDS(ReportQueryParam queryParam) {
-        QFilter loanBillQFilter = TradeFinanceFilterHelperExt.loanBillQFilter(queryParam);
-        // 本年末
-        // loanBillQFilter.and(new QFilter("bizdate", "<=", DateUtil.endOfYear(this.cutoffdate)));
+        QFilter loanBillQFilter = FinanceHelper.loanBillQFilter(queryParam);
+        // 查询截止日期
+        loanBillQFilter.and(new QFilter("bizdate", "<=", this.cutoffdate));
         // 本年初
         loanBillQFilter.and(new QFilter("bizdate", ">=", DateUtil.beginOfYear(this.cutoffdate)));
         QFilter elFilter = loanBillQFilter.copy();
@@ -197,10 +227,12 @@ public class FinancingClassificationStatisticsDataListPlugin extends AbstractRep
         String dataSource = paramMap.get("p8z9_filter_datasource").toString();
 
         // 债券发行
-        QFilter slCredFilter;
+        // QFilter slCredFilter;
         if (dataSource.contains(LoanTypeEnum.BOND.getValue())) {
-            slCredFilter = TradeFinanceRptHelper.getLoanTypeFilter(LoanTypeEnum.BOND.getValue());
-            bondQFilter.and(slCredFilter);
+            // slCredFilter = TradeFinanceRptHelper.getLoanTypeFilter(LoanTypeEnum.BOND.getValue());
+            // bondQFilter.and(slCredFilter);
+            QFilter bondCredFilter = TradeFinanceRptHelper.getBondCreditorFilter(paramMap);
+            bondQFilter.and(bondCredFilter);
             String zwfxSelectFields = this.getModelSelectFields(LoanTypeEnum.BOND.getValue());
             DataSet bondDS = QueryServiceHelper.queryDataSet(this.getClass().getName(), "cfm_loanbill_bond", zwfxSelectFields, bondQFilter.toArray(), null);
             dataDSList.add(bondDS);
--- code/tmc/p8z9-jtuat-tmc-cfm/src/main/java/p8z9/jtuat/tmc/cfm/plugin/report/FinancingClassificationStatisticsFormPlugin.java
+++ code/tmc/p8z9-jtuat-tmc-cfm/src/main/java/p8z9/jtuat/tmc/cfm/plugin/report/FinancingClassificationStatisticsFormPlugin.java
@@ -2,8 +2,6 @@ package p8z9.jtuat.tmc.cfm.plugin.report;
 
 import kd.bos.dataentity.entity.DynamicObject;
 import kd.bos.dataentity.entity.DynamicObjectCollection;
-import kd.bos.entity.datamodel.IDataModel;
-import kd.bos.entity.datamodel.events.PropertyChangedArgs;
 import kd.bos.entity.report.ReportQueryParam;
 import kd.bos.report.ReportList;
 import kd.bos.report.plugin.AbstractReportFormPlugin;
@@ -14,41 +12,10 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 /**
+ * 融资分类统计表（本年融资情况汇总表）
  * 报表界面插件
  */
 public class FinancingClassificationStatisticsFormPlugin extends AbstractReportFormPlugin implements Plugin {
-
-    @Override
-    public void propertyChanged(PropertyChangedArgs e) {
-        super.propertyChanged(e);
-
-        this.getControl("reportfilterap");
-        String name = e.getProperty().getName();
-        if ("p8z9_filter_dimension".equals(name)) {
-            String value_dimension = (String) e.getChangeSet()[0].getNewValue();
-            String[] hideFields = null;
-            String[] showFields = null;
-            switch (value_dimension) {
-                case "financing_model"://融资模式
-                case "credit_method"://增信方式
-                    hideFields = new String[]{"p8z9_filter_org"};
-                    break;
-                case "financing_subject"://融资主体
-                    showFields = new String[]{"p8z9_filter_org", "p8z9_filter_finproduct", "p8z9_filter_currencies"};
-                    break;
-            }
-            IDataModel model = this.getModel();
-            if (hideFields != null) {
-                for (String hideField : hideFields) {
-                    model.setValue(hideField, null);
-                }
-                this.getView().setVisible(false, hideFields);
-            }
-            if (showFields != null) {
-                this.getView().setVisible(true, showFields);
-            }
-        }
-    }
 
     @Override
     public void beforeQuery(ReportQueryParam queryParam) {
@@ -58,13 +25,13 @@ public class FinancingClassificationStatisticsFormPlugin extends AbstractReportF
         ReportList listTable = this.getView().getControl("reportlistap");
         switch (value_dimension) {
             case "financing_model":
-                fields = new String[]{"融资模式", "本位币", "融资余额（本位币）", "平均利率成本", "占比", "本金（本位币）", "利息（本位币）"};
+                fields = new String[]{"融资模式", "本位币", "融资余额（本位币）", "平均利率成本", "占比", "本金（本位币）", "利息（本位币）", "合计排序"};
                 break;
             case "financing_subject":
-                fields = new String[]{"融资主体", "本位币", "融资余额（本位币）", "平均利率成本", "占比", "本金（本位币）", "利息（本位币）"};
+                fields = new String[]{"融资主体", "本位币", "融资余额（本位币）", "平均利率成本", "占比", "本金（本位币）", "利息（本位币）", "合计排序"};
                 break;
             case "credit_method":
-                fields = new String[]{"担保方式", "本位币", "融资余额（本位币）", "占比"};
+                fields = new String[]{"担保方式", "本位币", "融资余额（本位币）", "占比", "合计排序"};
                 break;
         }
         ReportCommonHelper.rebuildColumn(fields, listTable);
@@ -73,30 +40,37 @@ public class FinancingClassificationStatisticsFormPlugin extends AbstractReportF
     @Override
     public void processRowData(String gridPK, DynamicObjectCollection dc, ReportQueryParam queryParam) {
         super.processRowData(gridPK, dc, queryParam);
+        // 初始化融资余额总额
+        BigDecimal totalBaseAmt = BigDecimal.ONE;
 
-        BigDecimal total_baseamt = dc.stream()
-                .map(dy -> dy.getBigDecimal("p8z9_baseamt"))
-                .reduce(BigDecimal.ZERO, BigDecimal::add);// 融资余额（本位币）总额
+        // 查找sumlevel为2的融资余额（代表为合计）
+        for (DynamicObject row : dc) {
+            if (row.getInt("p8z9_sumlevel") == 2) {
+                totalBaseAmt = row.getBigDecimal("p8z9_baseamt");
+                break; // 提前终止，提高效率
+            }
+        }
 
-        String value_dimension = (String) this.getModel().getValue("p8z9_filter_dimension");
-        switch (value_dimension) {
-            case "financing_model":
-            case "financing_subject":
-            case "credit_method":
-                for (DynamicObject dy : dc) {
-                    BigDecimal percentage = total_baseamt.compareTo(BigDecimal.ZERO) != 0
-                            ? dy.getBigDecimal("p8z9_baseamt").divide(total_baseamt, 10, RoundingMode.HALF_UP)
+        // 获取维度值
+        String valueDimension = (String) this.getModel().getValue("p8z9_filter_dimension");
+
+        // 判断是否需要计算占比
+        if ("financing_model".equals(valueDimension) ||
+                "financing_subject".equals(valueDimension) ||
+                "credit_method".equals(valueDimension)) {
+
+            boolean nonZeroTotal = totalBaseAmt.compareTo(BigDecimal.ZERO) != 0;
+
+            for (DynamicObject dy : dc) {
+                int sumLevel = dy.getInt("p8z9_sumlevel");
+                if (sumLevel != 2 && sumLevel != 1) { // 非合计行
+                    BigDecimal baseAmt = dy.getBigDecimal("p8z9_baseamt");
+                    BigDecimal percentage = nonZeroTotal
+                            ? baseAmt.divide(totalBaseAmt, 10, RoundingMode.HALF_UP)
                             : BigDecimal.ZERO;
-                    dy.set("p8z9_percentage", percentage);// 占比 = 融资余额（本位币） / 融资余额（本位币）总额
-
-                    // BigDecimal baseInterest = dy.getBigDecimal("p8z9_baseinterest");
-                    // BigDecimal basePrincipal = dy.getBigDecimal("p8z9_baseprincipal");
-                    // BigDecimal interestRate = basePrincipal.compareTo(BigDecimal.ZERO) != 0
-                    //         ? baseInterest.divide(basePrincipal, 10, RoundingMode.HALF_UP)
-                    //         : BigDecimal.ZERO;
-                    // dy.set("p8z9_avg_interestrate", interestRate);// 平均利率成本 = 利息（本位币） / 本金（本位币）
+                    dy.set("p8z9_percentage", percentage);
                 }
-                break;
+            }
         }
     }
 }
